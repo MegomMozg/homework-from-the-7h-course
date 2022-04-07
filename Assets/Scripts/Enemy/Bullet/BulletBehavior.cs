b8b8a9516f7dcf08ad21f369a40e3aa1bd4ca1ae
Markus <polayna05@mail.ru>
2022-04-07 15:53:25 +0300

    второе дз

---
+++ Assets/Scripts/Enemy/Bullet/BulletBehavior.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class BulletBehavior : MonoBehaviour
+    {
+        public Transform Bullet_transform;
+    }
+}
+
