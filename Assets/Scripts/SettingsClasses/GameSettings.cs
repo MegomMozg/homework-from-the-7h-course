f6fa84d93fd4a782b1a66507eabb8298f3198662
Markus <polayna05@mail.ru>
2022-04-21 15:02:52 +0300

    доделал скриптаблобджект для того чтобы вы могли понять что за что отвечает

--- Assets/Scripts/SettingsClasses/GameSettings.cs
+++ Assets/Scripts/SettingsClasses/GameSettings.cs
@@ -5,10 +5,14 @@ namespace PlatformerMVC
     [CreateAssetMenu(fileName = "Settings", menuName = "Other/Settings", order = 1)]
     public class GameSettings : ScriptableObject
     {
-        public bool Player;//+
-        public bool Background;//+
-        public bool DeathZone;//+
-        public bool Finish;//+
-        public bool Coin;//-
+        [Header("Тут можно настроить работу некоторых скриптов")]
+        [Space]
+        [Tooltip("отключение передвижения")] public bool Player;
+        [Tooltip("отключение слежения камеры и фона за игркоком")] public bool Background;//+
+        [Tooltip("отключение смерти")] public bool DeathZone;//+
+        [Tooltip("отключение победы")] public bool Finish;//+
+        [Tooltip("отключение монет(пока они вообще на работают)")] public bool Coin;//-
+
+        
     }
 }
